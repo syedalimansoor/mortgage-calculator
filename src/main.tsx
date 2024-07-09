092bceedb3a1521ad55948fba02e9c77f2958bc2
Syed Ali Mansoor <alimansoor4812@gmail.com>
2024-07-09 18:12:01 +0800

    feat: add base styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
+import "./styles/reset.css";
+import "./styles/fonts.css";
+import "./styles/variables.css";
+import "./styles/index.css";
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
